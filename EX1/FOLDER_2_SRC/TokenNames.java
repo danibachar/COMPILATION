1338c41280470d8d4f45363fc5f47384877c75e3
I <danibachar89@gmail.com>
2019-11-08 11:04:13 +0200

    EX1 init commit

---
+++ EX1/FOLDER_2_SRC/TokenNames.java
@@ -0,0 +1,15 @@
+public interface TokenNames {
+  /* terminals */
+  public static final int MINUS = 3;
+  public static final int DIVIDE = 5;
+  public static final int NUMBER = 9;
+  public static final int EOF = 0;
+  public static final int PLUS = 2;
+  public static final int ID = 10;
+  public static final int error = 1;
+  public static final int SEMICOLON = 8;
+  public static final int RPAREN = 7;
+  public static final int TIMES = 4;
+  public static final int LPAREN = 6;
+}
+
